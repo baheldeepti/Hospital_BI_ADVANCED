9eb3e876cbdb1b12f18ea0be22b0e294782bd720
baheldeepti <137839695+baheldeepti@users.noreply.github.com>
2025-09-22 00:15:15 -0700

    Update app.py

--- app.py
+++ app.py
@@ -534,7 +534,7 @@ def los_prep(data: pd.DataFrame):
 class CircuitBreaker:
     def __init__(self, failure_threshold=3, timeout=90):
         self.failure_threshold = failure_threshold
-               self.timeout = timeout
+        self.timeout = timeout
         self.failure_count = 0
         self.last_failure_time: Optional[datetime] = None
 
